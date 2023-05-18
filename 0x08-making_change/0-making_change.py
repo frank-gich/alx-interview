fe039a38c5a8fbc8dae56a60eba3064fe2038938
frank-gich <gichuhi5781@gmail.com>
2023-05-18 17:59:33 +0300

    unittest in js

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,9 +5,9 @@
 
 def makeChange(coins, total):
     """
-    return: the fewest number of coins needed to meet total
-        coins value of the coins in your posession
-        total the volume or the detemining factor
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
     """
     if not coins or coins is None:
         return -1
@@ -17,8 +17,8 @@ def makeChange(coins, total):
     coins = sorted(coins)[::-1]
     for coin in coins:
         while coin <= total:
-             total -= coin
-             change += 1
+            total -= coin
+            change += 1
         if (total == 0):
             return change
     return -1

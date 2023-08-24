de2ee46cd3a7cdd49ea20322eb2b5d0ad09e7d2c
frank-gich <gichuhi5781@gmail.com>
2023-08-24 12:51:28 +0300

    created a change making coins logic

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 
-"""
-making change function
-"""
+""" Contains makeChange function"""
+
 
 def makeChange(coins, total):
-    
-    if not coins in coins is none:
+    """
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
+    """
+    if not coins or coins is None:
         return -1
-   
-    if  total <= 0:
+    if total <= 0:
         return 0
-    change = 0 
+    change = 0
     coins = sorted(coins)[::-1]
     for coin in coins:
         while coin <= total:

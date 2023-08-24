685124d792c6bc189e3dcf29ddc9ea0b88477c22
frank-gich <104795834+frank-gich@users.noreply.github.com>
2023-08-24 12:14:22 +0300

    the making change chalenge

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+making change
+"""
+
+def makeChange(coins, total):
+    total = total
+    coins = 0
+    
+    if  (total =< 0):
+        return 0
+    
+    res = 
+    elif  ()        

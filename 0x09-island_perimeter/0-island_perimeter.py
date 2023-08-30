1232f5b1280f7dee6b3c1e9de38a35ddccac7387
frank-gich <gichuhi5781@gmail.com>
2023-08-30 13:44:34 +0300

    my island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+island perimeter
+"""
+
+def island_perimeter(grid):
+    """
+    calculate the perimeter of the island
+    Args:
+        grid: 2d list of integers contains 1-land and 0-water
+    Return:
+        the perimeter of the island
+    """
+    
+    p = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (grid[i][j] == 1):
+                if (i <= 0 or grid[i - 1][j] == 0):
+                    p += 1
+                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
+                    p += 1
+                if (j <= 0 or grid[i][j - 1] == 0):
+                    p += 1
+                if (j >= len(grid[i]) or grid[i][j + 1] == 0):
+                    p += 1
+    return p
